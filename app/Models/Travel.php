b38131a1b022b01119fcc33584f2311265b06282
BakkasBR <brahim.bakkas123@gmail.com>
2023-10-11 17:22:23 +0100

    first commit

---
+++ app/Models/Travel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Travel extends Model
+{
+    use HasFactory, HasSlug, HasUuids;
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days',
+    ];
+
+    public function tours() : HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function numberOfNights() : Attribute
+    {
+        return Attribute::make(
+            get: fn(int $value, array $attributes) => $attributes['number_of_days'] - 1
+        ); 
+    }
+}
